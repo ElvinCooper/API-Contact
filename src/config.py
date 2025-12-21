6c6e480e17f2895de72680bdf8e2adbc319a6d8b
Elvin cooper <ing.elvin01cooper@gmail.com>
2025-12-21 17:51:16 -0400

    fix: referencia a test-db por localhost

--- src/config.py
+++ src/config.py
@@ -61,7 +61,7 @@ class TestingConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "TEST_DATABASE_URI",
         #"postgresql://test_user:test_password@test-db:5432/test_contact"
-        "postgresql://test_user:test_password@test-db:5432/test_contact"
+        "postgresql://test_user:test_password@localhost:5432/test_contact"
     )
     
     
